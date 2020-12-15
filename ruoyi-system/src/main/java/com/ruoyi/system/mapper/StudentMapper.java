da894285263284e4c6f3753be361329cff221a7a
wsh <1746987387@qq.com>
2020-12-15 19:10:15 +0800

    更新mapper

---
+++ ruoyi-system/src/main/java/com/ruoyi/system/mapper/StudentMapper.java
@@ -0,0 +1,11 @@
+package com.ruoyi.system.mapper;
+
+import com.ruoyi.system.domain.Student;
+
+import java.util.List;
+
+public interface StudentMapper {
+
+    public List<Student> selectAllStudent();
+
+}
